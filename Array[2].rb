e27ac886c5991f5bf90f32701e2e082e3fd56bad
Piyush <piyushmeena4567@gmail.com>
2022-09-05 11:23:41 +0530

    Create Array[2].rb

---
+++ Array[2].rb
@@ -0,0 +1,20 @@
+# Initial data arrays
+Array1 = [1, 2, 3, 4]
+Array2 = ["Octa", "Nona", "Deca"]
+Array3 = ["Q", "QR", "QRS"]
+  
+# Initializing some elements 
+# which are to be pushed into arrays
+p = 5, 6
+q = "Undeca", "DoDeca", "TriDeca"
+r = "QRST", "QRSTU"
+  
+# Calling push() function
+A = Array1.push(p)                
+B = Array2.push(q)               
+C = Array3.push(r)  
+  
+# Printing the array of pushed element
+puts "#{A}"
+puts "#{B}"
+puts "#{C}"
