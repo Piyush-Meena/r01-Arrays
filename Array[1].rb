6b55ea0c23a23f068712c8605ae919a72e2835ad
Piyush <piyushmeena4567@gmail.com>
2022-09-05 11:20:59 +0530

    Create Array[1].rb

---
+++ Array[1].rb
@@ -0,0 +1,14 @@
+# Initial data arrays
+Array1 = [11, 22, 33, 44]
+Array2 = ["A", "B", "C"]
+Array3 = ["alpha", "beta", "gamma"]
+   
+# Calling push() function
+A = Array1.push(55, 66, 77)                
+B = Array2.push("D", "E", "F")               
+C = Array3.push("delta")  
+   
+# Printing the array of pushed element
+puts "#{A}"
+puts "#{B}"
+puts "#{C}"
